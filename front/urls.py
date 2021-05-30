7a7e2649c1867a263814bfc1a799d0e9106a5ac6
azat715 <azat715@gmail.com>
2021-05-30 21:35:24 +0500

    login views

--- front/urls.py
+++ front/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from front.views import QuizzesList
 
 app_name = "front"
 
 urlpatterns = [
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("", QuizzesList.as_view(), name="get_quizzes_list"),
 ]
--- front/views.py
+++ front/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class QuizzesList(TemplateView):
-
+class QuizzesList(LoginRequiredMixin, TemplateView):
+    login_url = "/login/"
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
