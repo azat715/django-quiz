a7a7f9d9e4d9880041c8887442a88f370058e4e6
azat715 <azat715@gmail.com>
2021-05-30 21:29:50 +0500

    откорректировал core/views.py

--- core/views.py
+++ core/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from core.models import Quiz
 from core.serializers import QuizSerializer
 
 
 @api_view()
-@permission_classes((permissions.AllowAny,))
-# @permission_classes([IsAuthenticated])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def get_quizzes(request):
     queryset = Quiz.objects.all()
     serializer = QuizSerializer(queryset, many=True)
--- front/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from front.views import QuizzesList
-
-app_name = "front"
-
-urlpatterns = [
-    path("", QuizzesList.as_view(), name="get_quizzes_list"),
-]
