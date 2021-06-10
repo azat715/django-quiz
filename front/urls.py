c99b9c832f1e441e645fc50b7d767accb8b6a3cc
azat715 <azat715@gmail.com>
2021-06-10 13:24:09 +0500

    front views

--- front/urls.py
+++ front/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from front.views import QuizzesList
+from front.views import QuizzesList, quiz
 
 app_name = "front"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("", QuizzesList.as_view(), name="get_quizzes_list"),
+    path("quizz/<str:slug>", quiz, name="get_quizzes_list"),
 ]
--- front/views.py
+++ front/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.views.generic.base import TemplateView
@@ -13,3 +14,18 @@ class QuizzesList(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context["title"] = "Викторина"
         return context
+
+
+class Quiz(LoginRequiredMixin, TemplateView):
+    login_url = "/login/"
+    template_name = "quiz.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Викторина"
+        return context
+
+
+@login_required
+def quiz(request, slug):
+    return render(request, "quiz.html", {"title": "Викторина", "slug": slug})
