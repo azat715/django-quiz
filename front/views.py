26e37ce53e71cfd710a9842d43af62e95ce2db54
notebook <azat175@gmail.com>
2021-06-11 23:13:27 +0500

    убрал лишний код

--- front/views.py
+++ front/views.py
@@ -16,16 +16,6 @@ class QuizzesList(LoginRequiredMixin, TemplateView):
         return context
 
 
-class Quiz(LoginRequiredMixin, TemplateView):
-    login_url = "/login/"
-    template_name = "quiz.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = "Викторина"
-        return context
-
-
 @login_required
 def quiz(request, slug):
     return render(request, "quiz.html", {"title": "Викторина", "slug": slug})
